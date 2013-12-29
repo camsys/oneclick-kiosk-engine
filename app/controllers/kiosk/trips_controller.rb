41fd7b8fc2b5d6cdfceaf8538b1d13558984514a
denishaskin <denis@constantorbit.com>
2013-12-29 15:04:48 -0500

    Mods to make engine work.  Still WIP.

---
+++ app/controllers/kiosk/trips_controller.rb
@@ -0,0 +1,79 @@
+module Kiosk
+  class TripsController < ::TripsController
+    def index
+    end
+
+    def show
+    end
+
+    def show_printer_friendly
+    end
+
+    def email
+    end
+
+    def email_provider
+    end
+
+    def email_itinerary
+    end
+
+    def email_feedback
+    end
+
+    def email_itinerary2_values
+    end
+
+    def email2
+    end
+
+    def details
+    end
+
+    def repeat
+    end
+
+    def edit
+    end
+
+    def unset_traveler
+    end
+
+    def set_traveler
+    end
+
+    def destroy
+    end
+
+    def new
+    end
+
+    def update
+    end
+
+    def create
+    end
+
+    def skip
+    end
+
+    def itinerary
+    end
+
+    def hide
+    end
+
+    def unhide_all
+    end
+
+    def select
+    end
+
+    def comments
+    end
+
+    def admin_comments
+    end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
-Rails.application.routes.draw do
+Kiosk::Engine.routes.draw do
+
+  resources :users do
+    resources :trips, :only => [:show, :index, :new, :create, :destroy, :edit, :update] do
+      collection do
+        post  'set_traveler'
+        get   'unset_traveler'
+        get   'search'
+        post  'geocode'
+      end
+      member do
+        get   'repeat'          
+        get   'select'
+        get   'details'
+        get   'itinerary'
+        post  'email'
+        post  'email_provider'
+        post  'email_itinerary'
+        get   'email_itinerary2_values'
+        post  'email2'
+        get   'hide'
+        get   'unhide_all'
+        get   'skip'
+        post  'rate'
+        post  'comments'
+        post  'admin_comments'
+        get   'edit_rating'
+        get   'email_feedback'
+        get   'show_printer_friendly'
+      end
+    end
+  end
 
 end
