75f6dc17db2fed328dde9255c65b7bc6cc1f686c
denishaskin <denis@constantorbit.com>
2013-12-30 18:02:25 -0500

    First page of trip planner renders, for most part. Address entry works.

--- app/controllers/kiosk/trips_controller.rb
+++ app/controllers/kiosk/trips_controller.rb
@@ -1,79 +1,116 @@
 module Kiosk
   class TripsController < ::TripsController
-    def index
-    end
 
-    def show
-    end
+    layout 'kiosk'
 
-    def show_printer_friendly
-    end
+    # def index
+    # end
 
-    def email
-    end
+    # def show
+    # end
 
-    def email_provider
-    end
+    # def show_printer_friendly
+    # end
 
-    def email_itinerary
-    end
+    # def email
+    # end
 
-    def email_feedback
-    end
+    # def email_provider
+    # end
 
-    def email_itinerary2_values
-    end
+    # def email_itinerary
+    # end
 
-    def email2
-    end
+    # def email_feedback
+    # end
 
-    def details
-    end
+    # def email_itinerary2_values
+    # end
 
-    def repeat
-    end
+    # def email2
+    # end
 
-    def edit
-    end
+    # def details
+    # end
 
-    def unset_traveler
-    end
+    # def repeat
+    # end
 
-    def set_traveler
-    end
+    # def edit
+    # end
 
-    def destroy
-    end
+    # def unset_traveler
+    # end
+
+    # def set_traveler
+    # end
+
+    # def destroy
+    # end
+
+    # def new
+    #   super
+    # end
 
     def new
-    end
 
-    def update
-    end
+      @trip_proxy = TripProxy.new()
+      @trip_proxy.traveler = @traveler
 
-    def create
-    end
+      # set the flag so we know what to do when the user submits the form
+      @trip_proxy.mode = MODE_NEW
 
-    def skip
-    end
+      # Set the travel time/date to the default
+      travel_date = default_trip_time
 
-    def itinerary
-    end
+      @trip_proxy.trip_date = travel_date.strftime(TRIP_DATE_FORMAT_STRING)
+      @trip_proxy.trip_time = travel_date.strftime(TRIP_TIME_FORMAT_STRING)
 
-    def hide
-    end
+      # Set the trip purpose to its default
+      @trip_proxy.trip_purpose_id = TripPurpose.all.first.id
 
-    def unhide_all
-    end
+      # default to a round trip. The default return trip time is set the the default trip time plus
+      # a configurable interval
+      return_trip_time = travel_date + DEFAULT_RETURN_TRIP_DELAY_MINS.minutes
+      @trip_proxy.is_round_trip = "1"
+      @trip_proxy.return_trip_time = return_trip_time.strftime(TRIP_TIME_FORMAT_STRING)
 
-    def select
-    end
+      # Create markers for the map control
+      @markers = create_trip_proxy_markers(@trip_proxy).to_json
+      @places = create_place_markers(@traveler.places)
 
-    def comments
+      respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @trip_proxy }
     end
+  end
 
-    def admin_comments
-    end
+    # def update
+    # end
+
+    # def create
+    # end
+
+    # def skip
+    # end
+
+    # def itinerary
+    # end
+
+    # def hide
+    # end
+
+    # def unhide_all
+    # end
+
+    # def select
+    # end
+
+    # def comments
+    # end
+
+    # def admin_comments
+    # end
 
   end
 end
